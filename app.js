cbb28f89555545935cc34311227b3c6a8a22879f
cesar magallon <cesar9mv@gmail.com>
2019-02-25 16:54:08 -0600

    Se agregaron funciones referentes a los modulos

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-// function greet() {
-//     console.log("hi");
-// }
+// // function greet() {
+// //     console.log("hi");
+// // }
+// // greet();
+// // let greet = () => console.log('hi');
+// // greet();
+// // function logGreeting(fn) {
+// //     fn();
+// // }
+// // let logGreeting = (fn) => fn();
+// // logGreeting(greet);
+// greet = () => console.log("hi");
 // greet();
-// let greet = () => console.log('hi');
-// greet();
-// function logGreeting(fn) {
-//     fn();
-// }
-// let logGreeting = (fn) => fn();
+// // logGreeting = (fn) => fn();
+// let logGreeting=(fn)=>fn();
 // logGreeting(greet);
-greet = () => console.log("hi");
+// greetMe = () =>  console.log("hi cesar"),
+// // let greetMe = function(){
+// //     console.log("hi cesar");
+// // }
+
+// greetMe();
+
+// logGreeting(greetMe);
+
+// logGreeting(() => console.log("hi world!"));
+
+var greet = require('./greet');
+
 greet();
-// logGreeting = (fn) => fn();
-let logGreeting=(fn)=>fn();
-logGreeting(greet);
-greetMe = () =>  console.log("hi cesar"),
-// let greetMe = function(){
-//     console.log("hi cesar");
-// }
-
-greetMe();
-
-logGreeting(greetMe);
-
-logGreeting(() => console.log("hi world!"));
-
-

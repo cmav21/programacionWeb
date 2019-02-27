6110a8fe2a6b666782e2b16e80a11bfbc44e5687
cesar magallon <cesar9mv@gmail.com>
2019-02-27 07:43:48 -0600

    correccion

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var person = {
-    firstname: 'John',
-    lastname: 'Doe',
-    greet: () => console.log(`Hello ${person.firstname} ${person.lastname}`)
-    // greet: function(){
-    //     console.log("Hello, " + this.firstname + ' ' + this.lastname);
-    // }
-};
+// var person = {
+//     firstname: 'John',
+//     lastname: 'Doe',
+//     greet: () => console.log(`Hello ${person.firstname} ${person.lastname}`)
+//     // greet: function(){
+//     //     console.log("Hello, " + this.firstname + ' ' + this.lastname);
+//     // }
+// };
 
 // person.greet();
 
@@ -16,7 +16,11 @@ function Person(firstname, lastname){
     this.lastname = lastname;
 }
 
-Person.prototype.greet = () => console.log('Hello! ' + this.firstname + ' ' + this.lastname);
+// Person.prototype.greet = () => console.log(`Hello! ${Person.firstname} ${Person.lastname}`);
+
+Person.prototype.greet = function() {
+    console.log("Hello " + this.firstname + ' ' + this.lastname);
+}
 
 var John = new Person('john', 'Doe');
 John.greet();

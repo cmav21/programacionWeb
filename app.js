213f00cfcad306221d20ffc418d2310c541acabd
cesar magallon <cesar9mv@gmail.com>
2019-03-06 08:15:17 -0600

    adding emmiter

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //Object properties and methods
+//we create an instance of emmiter
 var Emmiter = require('./emmiter')
 
 var emtr = new Emmiter();
-
+//adding events
 emtr.on('greet', function(){
     console.log("somewhere, someone said hello");
 })
@@ -11,10 +12,14 @@ emtr.on('greet', function(){
     console.log("A greeting occured!");
 })
 
+//execute that method
+emtr.emit('greet');
+
+//creating an object
 var obj = {
     greet: 'hello'
 }
-
+//acceding to the object's property by string index
 console.log(obj.greet);
 console.log(obj['greet']);
 var prop = 'greet';
@@ -23,6 +28,7 @@ console.log(obj[prop]);
 //functions and arrays
 
 var arr = [];
+//adding methods to the array
 
 arr.push(function(){
     console.log('Hello world!');

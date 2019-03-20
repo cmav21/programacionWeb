63d34bed9a5ff410fac25cec6a2ec9736f5de12b
20132867 CESAR AUGUSTO MAGALLON VADILLO <20132867@TELEMATICA.UCOL.MX>
2019-03-20 08:09:31 -0600

    clasesES6

--- app.js
+++ app.js
@@ -1,53 +1,26 @@
-// var person = {
-//     firstname: '',
-//     lastname: '',
-//     greet: function(){
-//         return `${this.firstname} ${this.lastname}`;
-//     }
+'use strict'
+
+class Person {
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    greet(){
+        console.log('Hello ' + this.firstname + ' ' + this.lastname);
+    }
+}
+
+// Person.prototype.greet = function(){
+//     console.log(`Hello, ${this.firstname} ${this.lastname}`);
 // }
 
-// var john = Object.create(person);
-// john.firstname = 'john';
-// john.lastname = 'doe';
+var John = new Person('john', 'Doe');
+John.greet();
 
-// console.log(john.greet());
+var jane = new Person('Jane', 'Doe');
+jane.greet();
 
-// var jane = Object.create(person);
-// jane.firstname = 'jane';
-// jane.lastname = 'doe';
-
-// console.log(jane.greet());
-
-var EventEmmiter = require('events');
-var util = require('util');
-
-function Greetr(){
-    EventEmmiter.call(this);    
-    this.greeting = 'Hello world';
-}
-
-util.inherits(Greetr, EventEmmiter);
-
-Greetr.prototype.greet = function(data){
-    console.log(this.greeting + ' ' + data);
-    this.emit('greet',data);
-}
-
-var  greeter1 = new Greetr();
-
-greeter1.on('greet', function(data){
-    console.log('someone greeted!: ' + data);
-});
-
-greeter1.greet('cesar');
-
-var obj = {
-    name: 'John Doe',
-    greet: function(param){
-        console.log(`Hello ${this.name}`);
-    }   
-}
-
-obj.greet();
-obj.greet.call({name: 'Jane Doe'});
-obj.greet.apply({name: 'Jane Doe'});
+console.log(John.__proto__);
+console.log(John.__proto__);
+console.log(John.__proto__ === jane.__proto__);

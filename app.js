5013e5479dd5c2d057d384298ceaff8cc4513b56
20132867 CESAR AUGUSTO MAGALLON VADILLO <20132867@TELEMATICA.UCOL.MX>
2019-03-20 07:54:06 -0600

    inheritEventEmitter

--- app.js
+++ app.js
@@ -18,27 +18,28 @@
 
 // console.log(jane.greet());
 
-// var EventEmmiter = require('events');
-// var util = require('util');
+var EventEmmiter = require('events');
+var util = require('util');
 
-// function Greetr(){
-//     this.greeting = 'Hello world';
-// }
+function Greetr(){
+    EventEmmiter.call(this);    
+    this.greeting = 'Hello world';
+}
 
-// util.inherits(Greetr, EventEmmiter);
+util.inherits(Greetr, EventEmmiter);
 
-// Greetr.prototype.greet = function(data){
-//     console.log(this.greeting + ' ' + data);
-//     this.emit('greet',data);
-// }
+Greetr.prototype.greet = function(data){
+    console.log(this.greeting + ' ' + data);
+    this.emit('greet',data);
+}
 
-// var  greeter1 = new Greetr();
+var  greeter1 = new Greetr();
 
-// greeter1.on('greet', function(data){
-//     console.log('someone greeted!: ' + data);
-// });
+greeter1.on('greet', function(data){
+    console.log('someone greeted!: ' + data);
+});
 
-// greeter1.greet('cesar');
+greeter1.greet('cesar');
 
 var obj = {
     name: 'John Doe',

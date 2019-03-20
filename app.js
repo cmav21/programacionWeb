85984bc5de8dc60c65af77c281cfd6782d42e5d7
20132867 CESAR AUGUSTO MAGALLON VADILLO <20132867@TELEMATICA.UCOL.MX>
2019-03-20 08:13:49 -0600

    cy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-'use strict'
+'use strict'//Para teber una sintaxis mas estricta
 
+//Declaracion de clase y todas las clases declaradas dentro estan en la cadena de prototipo
 class Person {
     constructor(firstname, lastname){
         this.firstname = firstname;
@@ -21,6 +22,7 @@ John.greet();
 var jane = new Person('Jane', 'Doe');
 jane.greet();
 
+//__proto__  es para ver la cadena de prototipo
 console.log(John.__proto__);
 console.log(John.__proto__);
 console.log(John.__proto__ === jane.__proto__);

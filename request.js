ee97f51720d48397e04a930b8b7671fecbf77414
20132867 CESAR AUGUSTO MAGALLON VADILLO <20132867@TELEMATICA.UCOL.MX>
2019-05-06 08:25:26 -0500

    [add]request

---
+++ request.js
@@ -0,0 +1,20 @@
+// let axios = require('axios');
+
+// axios.get('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/37.8267,-122.4233')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+let request = require('request');
+let celsiusToFahrenheit = require('celsius-to-fahrenheit');
+let fahrenheitToCelsius = require('fahrenheit-to-celsius');
+// request.get('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/37.8267,-122.4233')
+// .on('response', response => console.log(response))
+request('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/19.2433,-103.725', (err, res, body)=>{
+    let data = JSON.parse(body);
+    let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+    console.log(temperature);
+    console.log(data['currently']['temperature']);
+});

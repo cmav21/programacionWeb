cbdc235a2236e8a722f79e5f68be7f29a0d2b62c
cesar magallon <cesar9mv@gmail.com>
2019-05-13 08:37:49 -0500

    promises

--- nested-callbacks.js
+++ nested-callbacks.js
@@ -2,35 +2,80 @@ let request = require('request');
 let fahrenheitToCelsius = require('fahrenheit-to-celsius');
 
 request('https://us1.locationiq.com/v1/search.php?key=44fcebb8c5c660&q=Colima&format=json', function(err, res, body){
-    if(err)
-    console.log(err);
-    else {
-        let data = JSON.parse(body);
-        let latitud = data[0]['lat'];
-        let longitud = data[0]['lon']
-        console.log(`latitud:${latitud}, longitud:${longitud}`);
-        request(`https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/${latitud},${longitud}`, function(err, res, body){
-            if(err)
-            console.log(err);
-            else 
-            {
-                let data = JSON.parse(body);
-                let temperature = fahrenheitToCelsius(data['currently']['temperature']);
-                console.log(temperature + '°C');                 
-            }
+    promise1 = new Promise((resolve, reject)=>{
+        if(body){
+            let data = JSON.parse(body);
+            let latitud = data[0]['lat'];
+            let longitud = data[0]['lon']
+            resolve([latitud, longitud]);
+        } else{
+            reject(err)
+        }
+    }).then(coordenadas => {
+        console.log(coordenadas);
+        request(`https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/${coordenadas[0]},${coordenadas[1]}`, function(err, res, body){
+            promise2 = new Promise((resolve, reject) => {
+                if(body){
+                    let data = JSON.parse(body);
+                    let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+                    resolve(`${temperature} °C`);
+                } else{
+                    reject(err)
+                }
+            }).then(data => console.log(data)).catch(err => console.log(err))
         })
         let options = { method: 'GET',
-        url: 'https://api.openuv.io/api/v1/uv',
-        qs: { lat: `${latitud}`, lng: `${longitud}`},
-        headers: 
-         { 'content-type': 'application/json',
-           'x-access-token': 'ee6fef521196eaa60d6e79743993c111' } };
+            url: 'https://api.openuv.io/api/v1/uv',
+            qs: { lat: `${coordenadas[0]}`, lng: `${coordenadas[1]}`},
+            headers: 
+            { 'content-type': 'application/json',
+            'x-access-token': 'ee6fef521196eaa60d6e79743993c111' } };
        
-       request(options, function (error, response, body) {
-         if (error) throw new Error(error);
-         let data = JSON.parse(body);
-         console.log(data['result']['uv'] + '°');
-       });
-    }
-});
+        request(options, function (error, response, body) {
+            promise3 = new Promise((resolve, reject) => {
+                if(body){
+                    let data = JSON.parse(body);
+                    resolve(data['result']['uv'] + '°');
+                } else {
+                    reject(err);
+                }
+            }).then(data => console.log(data)).catch(err => console.log(err))
+         });
+
+    }).catch(err => console.log(err))
+})
+
+
+
+//     if(err)
+//     console.log(err);
+//     else {
+//         let data = JSON.parse(body);
+//         let latitud = data[0]['lat'];
+//         let longitud = data[0]['lon']
+//         console.log(`latitud:${latitud}, longitud:${longitud}`);
+//         request(`https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/${latitud},${longitud}`, function(err, res, body){
+//             if(err)
+//             console.log(err);
+//             else 
+//             {
+//                 let data = JSON.parse(body);
+//                 let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+//                 console.log(temperature + '°C');                 
+//             }
+//         })
+//         let options = { method: 'GET',
+//         url: 'https://api.openuv.io/api/v1/uv',
+//         qs: { lat: `${latitud}`, lng: `${longitud}`},
+//         headers: 
+//          { 'content-type': 'application/json',
+//            'x-access-token': 'ee6fef521196eaa60d6e79743993c111' } };
+       
+//        request(options, function (error, response, body) {
+//          if (error) throw new Error(error);
+//          let data = JSON.parse(body);
+//          console.log(data['result']['uv'] + '°');
+//        });
+//     }
+// });
 
--- request.js
+++ request.js
@@ -12,11 +12,31 @@ let celsiusToFahrenheit = require('celsius-to-fahrenheit');
 let fahrenheitToCelsius = require('fahrenheit-to-celsius');
 // request.get('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/37.8267,-122.4233')
 // .on('response', response => console.log(response))
-request('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/19.2433,-103.725', (err, res, body)=>{
-    let data = JSON.parse(body);
-    let temperature = fahrenheitToCelsius(data['currently']['temperature']);
-    console.log(temperature);
-    console.log(data['currently']['temperature']);
+// request('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/19.2433,-103.725', (err, res, body)=>{
+//     let data = JSON.parse(body);
+//     let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+//     console.log(temperature);
+//     console.log(data['currently']['temperature']);
+// });
+
+// request('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/19.2433,-103.725', (err, res, body)=>{
+//     let data = JSON.parse(body);
+//     let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+//     console.log(temperature);
+//     console.log(data['currently']['temperature']);
+// });
+
+request('https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/19.2433,-103.725', (err, res, body) => {
+    promise = new Promise((res, rej)=>{
+        if(body){
+            let data = JSON.parse(body);
+        let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+            res(data['currently']['temperature']);
+        } else {
+            rej(err);
+        }
+    }).then(data => console.log(data)).catch(e => console.log(e))
+    
 });
 
 //api de google

77e853bdfe71abfe48ebadbf8bdc53d311df9dd4
cesar magallon <cesar9mv@gmail.com>
2019-05-13 06:58:13 -0500

    [add]nested callbacks

--- nested-callbacks.js
+++ nested-callbacks.js
@@ -1,4 +1,5 @@
 let request = require('request');
+let fahrenheitToCelsius = require('fahrenheit-to-celsius');
 
 request('https://us1.locationiq.com/v1/search.php?key=44fcebb8c5c660&q=Colima&format=json', function(err, res, body){
     if(err)
@@ -7,14 +8,15 @@ request('https://us1.locationiq.com/v1/search.php?key=44fcebb8c5c660&q=Colima&fo
         let data = JSON.parse(body);
         let latitud = data[0]['lat'];
         let longitud = data[0]['lon']
-        console.log(data);
+        console.log(`latitud:${latitud}, longitud:${longitud}`);
         request(`https://api.darksky.net/forecast/98c6dde1e30901cf32db92c65f79fac9/${latitud},${longitud}`, function(err, res, body){
             if(err)
             console.log(err);
             else 
             {
                 let data = JSON.parse(body);
-                console.log(data);                 
+                let temperature = fahrenheitToCelsius(data['currently']['temperature']);
+                console.log(temperature + '°C');                 
             }
         })
         let options = { method: 'GET',
@@ -27,7 +29,7 @@ request('https://us1.locationiq.com/v1/search.php?key=44fcebb8c5c660&q=Colima&fo
        request(options, function (error, response, body) {
          if (error) throw new Error(error);
          let data = JSON.parse(body);
-         console.log(data);
+         console.log(data['result']['uv'] + '°');
        });
     }
 });
